fe6c0a4cfbb78098dcf1cb21071f2a2f457ce09c
vivekvidyarthi99 <31702922+vivekvidyarthi99@users.noreply.github.com>
2017-09-28 22:25:37 -0400

    Initial commit

---
+++ TW-04/js/gsearch.js
@@ -0,0 +1,11 @@
+
+(function() {
+    var cx = '009530801423899025858:yu771-jhsi0';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
+
